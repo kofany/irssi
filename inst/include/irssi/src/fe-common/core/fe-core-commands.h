3d70e60f43de62396848befbea9e6bee2d5753bf
kofany <j@dabrowski.biz>
2025-10-12 14:45:21 +0200

    checkpoint [14:45:21 12/10/2025]

--- inst/include/irssi/src/fe-common/core/fe-core-commands.h
+++
@@ -1,9 +0,0 @@
-#ifndef IRSSI_FE_COMMON_CORE_FE_CORE_COMMANDS_H
-#define IRSSI_FE_COMMON_CORE_FE_CORE_COMMANDS_H
-
-extern int command_hide_output;
-
-void fe_core_commands_init(void);
-void fe_core_commands_deinit(void);
-
-#endif
